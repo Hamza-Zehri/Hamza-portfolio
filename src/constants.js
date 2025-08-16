9f5cd9055d8728c9d05a8cde5dcfb61b89e7e96a
Hamza-Zehri <hamidzehri42@gmail.com>
2025-08-16 12:32:17 +0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,262 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import adobelightroomlogo from './assets/tech_logo/adobe-lightroom-icon.png';
+import adobephotoshoplogo from './assets/tech_logo/adobe-photoshop-icon.png';
+import autocadlogo from './assets/tech_logo/autocad-icon.png';
+import canvalogo from './assets/tech_logo/canva-icon.png';
+import sketchuplogo from './assets/tech_logo/sketchup-icon.png';
+import VrayLogo from './assets/tech_logo/Vray-icon.png';
+import upworkLogo from './assets/tech_logo/upwork-icon.png';
+
+// Experience Section Logo's
+import buetkLogo from './assets/company_logo/buetk_logo.png';
+import navttcLogo from './assets/company_logo/navttc_logo.png';
+import aetikLogo from './assets/company_logo/aetik_logo.png';
+
+// Education Section Logo's
+import buetLogo from './assets/education_logo/buet_logo.png';
+import gbdLogo from './assets/education_logo/gbd_logo.png';
+import srmLogo from './assets/education_logo/srm_logo.png';
+import afsLogo from './assets/education_logo/afs_logo.png';
+
+// Project Section Logo's
+import tmsLogo from './assets/work_logo/tms.png';
+import CGPALogo from './assets/work_logo/Cgpa_Calculator.png';
+import dataHUBLogo from './assets/work_logo/data_entry.png';
+import BuetPortalLogo from './assets/work_logo/buet_portal.png';
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Adobe Photoshop', logo: adobephotoshoplogo },
+      { name: 'Adobe Lightroom', logo: adobelightroomlogo },
+      { name: 'Canva', logo: canvalogo },
+      { name: 'UpWork', logo: upworkLogo },
+      { name: 'AutoCAD', logo: autocadlogo },
+      { name: 'Sketchup', logo: sketchuplogo },
+      { name: 'V-ray', logo: VrayLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: buetkLogo,
+      role: "Fullstack Developer",
+      company: "Balochistan University of Enigeering & Technology Khuzdar",
+      date: "April 2024 - August 2025",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: navttcLogo,
+      role: "AutoCAD With Revit",
+      company: "ational Vocational & Technical Training Commission",
+      date: "March 2022 - Sepetember 2022",
+      desc: "Completed a vocational training program focusing on architectural and interior design software. Gained proficiency in **AutoCAD 2D/3D** for drafting and design, **3ds Max** for 3D modeling, and **V-Ray** for photorealistic rendering. Developed skills in creating detailed blueprints and high-quality visualizations for a variety of projects.",
+      skills: [
+        "AutoCAD 2D/3D",
+    "3ds Max",
+    "V-Ray",
+    "Revit"
+      ],
+    },
+    {
+      id: 2,
+      img: aetikLogo,
+      role: "Office Automation & Typing",
+      company: "Apple Computer Trainings Center Khuzdar",
+      date: "August 2020 - December 2020",
+      desc: "Proficient in MS Office Suite with a strong emphasis on MS Word, MS Excel, and MS PowerPoint. Mastered data entry and composing techniques in both English and Urdu, achieving typing speeds of 45 words per minute in both languages. Also skilled in printing techniques and using Urdu InPage software.",
+      skills: [
+         "MS Word",
+    "MS Excel",
+    "MS PowerPoint",
+    "MS Office",
+    "Urdu InPage",
+    "Composing Techniques",
+    "Data Entry",
+    "Printing Techniques",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: buetLogo,
+      school: "Balochistan University of Engineering & Technology Khuzdar",
+      date: "Dec 2022 - Dec 2026",
+      grade: "3.5 CGPA (in process)",
+      desc: "Currently pursuing a Bachelor of Engineering in Software Engineering. This program has provided me with a robust foundation in software development principles and practices, including **Data Structures, Algorithms, Object-Oriented Programming, and Database Management Systems**. I am gaining practical experience in designing and developing software solutions while honing my skills in various programming languages and modern software development methodologies.",
+      degree: "Bachelor of Engineering (B.E) in Software Engineering",
+    },
+    {
+      id: 1,
+      img: gbdLogo,
+      school: "GOVT: Boys Degree Khuzdar",
+      date: "Sept 2020 - July 2022",
+      grade: "82.5%",
+      desc: "Completed my F.Sc Pre-Engineering, building a strong academic foundation in core science subjects. The curriculum included advanced topics in Physics, Chemistry, and Mathematics, which prepared me for future studies in engineering and technology. This period focused on analytical thinking and problem-solving skills, which have been instrumental in my academic and professional journey.",
+      degree: "F.Sc Pre-Engineering",
+    },
+    {
+      id: 2,
+      img: srmLogo,
+      school: "Sun Rise Model Public High School Khuzdar",
+      date: "March 2019 - July 2020",
+      grade: "81.4%",
+      desc: "Completed my 9th and 10th grade studies, achieving a strong academic foundation in various subjects. This period was crucial for developing my core knowledge and study habits.",
+      degree: "Matriculation (9th & 10th Grade)",
+    },
+    {
+      id: 3,
+      img: afsLogo,
+      school: "Al-Farooq Resdential School Khuzdar",
+      date: "March 2013 - March 2018",
+      grade: "85%",
+      desc: "Completed my middle school education, establishing a foundational understanding of core subjects. This period focused on building strong learning habits and preparing for higher academic levels",
+      degree: "Middle School Education (3th - 8th Grade)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Tailor Measurement System",
+      description:
+        "This project is a Tailor Measurement Management System focused on customer management. It provides functionalities to manage customer data, including creating, retrieving, updating, and deleting customer information.",
+      image: tmsLogo,
+      tags: ["HTML", "CSS", "Typescript",],
+      github: "https://github.com/Hamza-Zehri/T-M-S",
+      webapp: "https://t-m-s-92aa.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "GPA & CPGA Calculator",
+      description:
+        "This is a simple, modern, and user-friendly web-based calculator for determining your Semester GPA and cumulative CGPA. It's built with clean HTML, CSS, and JavaScript, designed to be intuitive and accessible for students.",
+      image: CGPALogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Hamza-Zehri/GPA-CGPA-Calculator",
+      webapp: "https://gpa-cgpa-calculator-fawn.vercel.app",
+    },
+    {
+      id: 2,
+      title: "Ledger Data Repository",
+      description:
+        "Ledger Data Repository This repository contains a collection of CSV files extracted from a larger ledger spreadsheet. It's designed to provide a structured and easily accessible record of financial or transactional data, broken down by individual participants and a general overview.",
+      image: dataHUBLogo,
+      tags: ["MS Excel", "CSV", "Data Management"],
+      github: "https://github.com/Hamza-Zehri/Ledger-Data-Hub",
+      webapp: "https://docs.google.com/spreadsheets/d/1Mxs4MFhhiac4JzAC82YjAWitt3rZ7pWm/edit?usp=drive_link&ouid=110859360713305240096&rtpof=true&sd=true",
+    },
+    {
+      id: 3,
+      title: "BUET Student Portal",
+      description:
+        "A comprehensive web-based student portal for Balochistan University of Engineering & Technology (BUET) with modern UI design and full-stack functionality.",
+      image: BuetPortalLogo,
+      tags: ["HTML", "CSS", "JavaScript", "MongoDB", "Express", "bcryptjs", "Node.js","JWT", "PDFMake", "Mongoos", "CORS","Git",],
+      github: "https://github.com/Hamza-Zehri/University_Portal",
+      webapp: "https://buetk-portal.vercel.app",
+    },
+    
+  ];
