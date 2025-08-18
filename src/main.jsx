16cdb27352805a2c7d1db539c192d614f9e9be25
Hamza-Zehri <hamidzehri42@gmail.com>
2025-08-18 15:17:34 +0500

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
